4953bd9808a3bc900e63b2e548a9f86676865519
Dave Barnett <dave.barnett@redsensors.com>
2018-01-01 19:10:45 +0000

    Initial Commit.
    Player, Ground, Pickups and walls.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+
+	Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		float inHorizontal = Input.GetAxis("Horizontal");
+		float inVertical = Input.GetAxis("Vertical");
+
+		Vector3 force = new Vector3(inHorizontal, 0, inVertical);
+		rb.AddForce(force * speed);
+	}
+}
