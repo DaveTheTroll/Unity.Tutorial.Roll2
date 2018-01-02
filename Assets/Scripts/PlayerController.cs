462ea838e1c752e9179d0d046a9f52e29415b31f
Dave Barnett <dave.barnett@redsensors.com>
2018-01-02 16:45:07 +0000

    UI Text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
 	public float speed;
 
+	public Text collectedItemsText;
+	public Text winText;
+
 	Rigidbody rb;
+	int collectedItems;
 
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
+		collectedItems = 0;
+		UpdateUI();
+		winText.text = "";
+	}
+
+	void UpdateUI()
+	{
+		collectedItemsText.text = string.Format("Count: {0}", collectedItems);
+		if (collectedItems == 12)
+		{
+			winText.text = "FINISHED!";
+		}
 	}
 
 	void FixedUpdate()
@@ -27,6 +44,8 @@ public class PlayerController : MonoBehaviour
 		if (other.gameObject.CompareTag("Pickup"))
 		{
 			other.gameObject.SetActive(false);
+			collectedItems++;
+			UpdateUI();
 		}
 	}
 }
